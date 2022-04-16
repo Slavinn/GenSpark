48dd2fd5d2f97064244b9a3bcfc156392aa874b9
Eliot vanheumen <eliot.vanheumen@gmail.com>
2022-04-16 09:14:27 -0700

    skeleton

--- HangMan2.0/src/com/company/Hangman.java
+++ HangMan2.0/src/com/company/Hangman.java
@@ -4,7 +4,46 @@ public class Hangman {
     private String randomString;
     private String guessingString;
     private int score;
-    privt int
+    private ArrayList<String> hangMan;
 
+    public Hangman() {
+        this.randomString = "";
+        this.guessingString = "";
+        this.score = 0;
+        this.hangMan = new ArrayList<>();
+    }
 
+    public String getRandomString() {
+        return randomString;
+    }
+
+    public void setRandomString(String randomString) {
+        this.randomString = randomString;
+    }
+
+    public String getGuessingString() {
+        return guessingString;
+    }
+
+    public void setGuessingString(String guessingString) {
+        this.guessingString = guessingString;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public ArrayList<String> getHangMan() {
+        return hangMan;
+    }
+
+    public void setHangMan(ArrayList<String> hangMan) {
+        this.hangMan = hangMan;
+    }
+
+    public void randomStringHas
 }
