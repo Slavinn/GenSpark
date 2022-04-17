6343e9d2974f6c52e2f84851bde48fdd4f059759
eliot vanheumen <eliot.vanheumen@gmail.com>
2022-04-17 11:46:59 -0700

    issue with filling in guessingString with streams duplicates aren't being added

--- HangMan2.0/src/com/company/Hangman.java
+++ HangMan2.0/src/com/company/Hangman.java
@@ -1,16 +1,52 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Hangman {
     private String randomString;
-    private String guessingString;
-    private int score;
+    private StringBuilder guessingString;
+    private String wrongGuesses;
     private ArrayList<String> hangMan;
+    private ArrayList<String> scoreBoard;
+    private String name;
+    private int score;
+    private File highscores = new File("src/Gamefiles/highScores.txt");
+
 
     public Hangman() {
-        this.randomString = "";
-        this.guessingString = "";
+        setRandomString("Tree");
+        setGuessingString();
+        this.wrongGuesses = "";
         this.score = 0;
         this.hangMan = new ArrayList<>();
+        this.name = "";
+    }
+
+    public void setHangMan() {
+        try {
+            Stream<String> fileStream = Files.lines(Paths.get("src/Gamefiles/assets.txt"));
+            this.hangMan = (ArrayList<String>) fileStream.collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    public String getWrongGuesses() {
+        return wrongGuesses;
+    }
+
+    public void setWrongGuesses(char wrongGuesses) {
+        this.wrongGuesses += wrongGuesses;
     }
 
     public String getRandomString() {
@@ -18,15 +54,51 @@ public class Hangman {
     }
 
     public void setRandomString(String randomString) {
-        this.randomString = randomString;
+        this.randomString = randomString.toLowerCase();
     }
 
-    public String getGuessingString() {
+    public StringBuilder getGuessingString() {
         return guessingString;
     }
 
-    public void setGuessingString(String guessingString) {
-        this.guessingString = guessingString;
+    public void setGuessingString() {
+        int l = getRandomString().length();
+        String str = new String(new char[l]).replace('\0', '_');
+        this.guessingString = new StringBuilder(str);
+    }
+
+    public void setGuessingString(int index, char letter) {
+        this.guessingString.setCharAt(index, letter);
+    }
+
+    public void getScores() {
+        try {
+            if (!this.highscores.exists()) {
+                this.highscores.createNewFile();
+            }
+            Stream<String> fileStream = Files.lines(Paths.get(this.highscores.getPath()));
+            System.out.println("Top scores: ");
+            fileStream.sorted((s1, s2) -> s1.split(" ")[0].compareTo(s2.split(" ")[0])).forEach(System.out::println);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void addScores() {
+        try{
+            Files.writeString(this.highscores.toPath(), String.format("%s %s", getScore(),getName()));
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getScore() {
@@ -34,7 +106,7 @@ public class Hangman {
     }
 
     public void setScore(int score) {
-        this.score = score;
+        this.score += score;
     }
 
     public ArrayList<String> getHangMan() {
@@ -45,5 +117,76 @@ public class Hangman {
         this.hangMan = hangMan;
     }
 
-    public void randomStringHas
+    public int randomStringHasCharacter(char letter) {
+        return this.randomString.indexOf(letter);
+    }
+
+    private void setHangMan(String string, int index) {
+        this.hangMan.set(index, string);
+    }
+
+    public boolean compareStrings() {
+        return getRandomString().equals(getGuessingString().toString());
+    }
+
+
+    public void buildHangMan(int guesses) {
+
+        switch (guesses) {
+            case 1:
+                setHangMan("  O   |", guesses);
+                break;
+            case 2:
+            case 3:
+                setHangMan("  |   |", guesses);
+                break;
+            case 4:
+                setHangMan(" /    |", guesses);
+                break;
+            case 5:
+                setHangMan(" / \\  |", 4);
+                break;
+            case 6:
+                setHangMan(" \\|   |", 2);
+                break;
+            case 7:
+                setHangMan(" \\|/  |", 2);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public String fetchRandomWord() {
+        try {
+            URL connection = new URL("https://random-word-api.herokuapp.com/word?number=1&swear=0");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) connection.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            int responseCode = httpURLConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                //Set RandomString to result
+                return response.toString();
+            } else {
+                throw new Exception("Error fetching word from API");
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
 }
--- HangMan2.0/src/com/company/Main.java
+++ HangMan2.0/src/com/company/Main.java
@@ -1,13 +1,65 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 	// write your code here
-        while(true) {
+        Hangman game = new Hangman();
+        game.setHangMan();
+        int index = 0;
+        int guesses = 0;
+        boolean newGame = false;
+        System.out.println("Please enter name for the scoreBoard");
+        game.setName(scanner.nextLine());
+        while(!newGame) {
+            try {
 
+                game.getHangMan().stream().forEach(System.out::println);
+                System.out.println("Missed letters: " + game.getWrongGuesses());
+                System.out.println(game.getGuessingString());
+                System.out.println("Guess a letter:");
+                char guessChar  = scanner.nextLine().charAt(0);
+
+                if((index = game.randomStringHasCharacter(guessChar)) > -1) {
+                    game.setGuessingString(index, guessChar);
+                    game.setScore(10 - guesses);
+                } else {
+                    guesses++;
+                    game.buildHangMan(guesses);
+                   game.setWrongGuesses(guessChar);
+                }
+                if(guesses >= 7) {
+                    String lost = String.format("You lost :(! The secret word is \"%s\"! Better luck next time!", game.getRandomString());
+                    System.out.println(lost);
+                    newGame = true;
+                } else if (game.compareStrings()) {
+                    String winner = String.format("Yes! The secret word is \"%s\"! You have won!", game.getRandomString());
+                    System.out.println(winner);
+                    newGame = true;
+                }
+                if (newGame) {
+                    System.out.println("You scored: " + game.getScore());
+                    game.addScores();
+                    game.getScores();
+                    System.out.println("Do you want to play again? (yes or no)");
+                    String output = scanner.nextLine();
+                    if (output.equalsIgnoreCase("yes")) {
+                        game = new Hangman();
+                        System.out.println("Please enter name for the scoreBoard");
+                        game.setName(scanner.nextLine());
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
+
     }
+
+
 }
